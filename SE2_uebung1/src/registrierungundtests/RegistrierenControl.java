fc644a8e1f8006c392a5907d0adb2cc6f853591e
hedwigmiau <nweigel97@gmail.com>
2018-04-25 01:12:19 +0200

    email regex Ausdruck hinzugefügt

--- SE2_uebung1/src/registrierungundtests/RegistrierenControl.java
+++ SE2_uebung1/src/registrierungundtests/RegistrierenControl.java
@@ -1,5 +1,5 @@
 package registrierungundtests;
-
+import java.util.regex.*;
 public class RegistrierenControl {
 	//beim registrieren viele möglichkeiten von fehlern
 //	bsp Userfactory.createDefaultUserWithNoPassword...   zum erstellen eines zu testenden falles  -> problem habe mega vile funktionen in factory
@@ -17,11 +17,21 @@ public class RegistrierenControl {
 	}
 	private static boolean checkUserDTO(UserDTO u){
 		if(u.getEmail_pflicht()!=null&u.getName_pflicht()!=null&u.getPasswort_pflicht()!=null){
-			//Check später mit regex etc.
-			return true;
+//			System.out.println(checkEmail(u.getEmail_pflicht()));
+//			System.out.println(u.getEmail_pflicht());
+			return checkEmail(u.getEmail_pflicht())&&true;
 		}else{
 			return false;
 		}
+	}
+	private static boolean checkEmail(String email){
+		Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+        Matcher mat = pattern.matcher(email);
+        if(mat.matches()){
+            return true;
+        }else{
+            return false;
+        }
 	}
 	private static void speichere(UserDTO u){
 		
